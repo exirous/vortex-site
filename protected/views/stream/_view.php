9f3d8eebafc9c6e273073308b21b1257d4209092
Rusinov Maxim <rusmaxim@gmail.com>
2012-09-24 02:02:26 +0600

    StreamFix

--- protected/views/stream/_view.php
+++ protected/views/stream/_view.php
@@ -6,7 +6,7 @@
         <param name="allowScriptAccess" value="always" />
         <param name="allowNetworking" value="all" />
         <param name="movie" value="http://ru.twitch.tv/widgets/live_embed_player.swf" />
-        <param name="flashvars" value="hostname=ru.twitch.tv&channel=rusmaxim&auto_play=false&start_volume=25" />
+        <param name="flashvars" value="hostname=ru.twitch.tv&channel=<?php echo CHtml::encode($data->channel); ?>&auto_play=false&start_volume=25" />
     </object>
     <br />
     <span class="<?php echo $data->owner->mainCharacter->warcraftClass->english_name;?>"><?php echo $data->owner->mainCharacter->name; ?></span>
